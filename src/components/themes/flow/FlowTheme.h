f193ebc7809f6d5ebd8418a20e763d77436e7c9b
ideo2004-afk <ideo2004@gamil.com>
2026-03-13 14:21:23 +0800

    perf: reduce Flow theme book carousel count to 7

--- src/components/themes/flow/FlowTheme.h
+++ src/components/themes/flow/FlowTheme.h
@@ -24,7 +24,7 @@ constexpr ThemeMetrics values = {.batteryWidth = 16,
                                  .homeTopPadding = 56,
                                  .homeCoverHeight = 314,      // 25-kai book ratio (~0.7)
                                  .homeCoverTileHeight = 360,  // Cover + Title area
-                                 .homeRecentBooksCount = 10,   // Up to 10 books in carousel
+                                 .homeRecentBooksCount = 7,   // Up to 7 books in carousel
                                  .buttonHintsHeight = 40,
                                  .sideButtonHintsWidth = 30,
                                  .progressBarHeight = 16,
